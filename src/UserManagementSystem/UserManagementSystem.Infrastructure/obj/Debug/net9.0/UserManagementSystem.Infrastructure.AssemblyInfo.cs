4c68cba1117f3d87034c0450cad7376630a7e6c7
unknown <balasumon18@gmail.com>
2025-09-19 13:49:11 +0600

    add database script

--- src/UserManagementSystem/UserManagementSystem.Infrastructure/obj/Debug/net9.0/UserManagementSystem.Infrastructure.AssemblyInfo.cs
+++ src/UserManagementSystem/UserManagementSystem.Infrastructure/obj/Debug/net9.0/UserManagementSystem.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagementSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc4d945eb32be64cae5a276639b637af6faaf575")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21954454c1abd891b73d0ddfa4536bbee745d381")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagementSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagementSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
